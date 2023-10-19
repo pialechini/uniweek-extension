31f9be34365e3381354a3350f03361d7a82be4b3
PialeChini <mobinzohourian@gmail.com>
2023-10-19 13:21:38 +0330

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Uniweek Browser Extension
---
+++ manifest.ts
@@ -0,0 +1,46 @@
+import packageJson from "./package.json";
+
+/**
+ * After changing, please reload the extension at `chrome://extensions`
+ */
+const manifest: chrome.runtime.ManifestV3 = {
+  manifest_version: 3,
+  name: packageJson.name,
+  version: packageJson.version,
+  description: packageJson.description,
+  permissions: ["storage"],
+  options_page: "src/pages/options/index.html",
+  background: {
+    service_worker: "src/pages/background/index.js",
+    type: "module",
+  },
+  // action: {
+  //   default_popup: "src/pages/popup/index.html",
+  //   default_icon: "icon-34.png",
+  // },
+  // chrome_url_overrides: {
+  //   newtab: "src/pages/newtab/index.html",
+  // },
+  icons: {
+    "16": "icon-16.png",
+    "32": "icon-32.png",
+    "48": "icon-48.png",
+    "128": "icon-128.png",
+  },
+  content_scripts: [
+    {
+      matches: ["http://*/*", "https://*/*", "<all_urls>"],
+      js: ["src/pages/content/index.js"],
+      // KEY for cache invalidation
+      css: ["assets/css/contentStyle<KEY>.chunk.css"],
+    },
+  ],
+  web_accessible_resources: [
+    {
+      resources: ["assets/js/*.js", "assets/css/*.css", "icon-16.png", "icon-32.png", "icon-48.png", "icon-128.png"],
+      matches: ["*://*/*"],
+    },
+  ],
+};
+
+export default manifest;
